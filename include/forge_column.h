0a0579eec70d27a3da1aa676752738d6676e0c98
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:30:06 +0200

    feat: expose column min and max kernels

--- include/forge_column.h
+++ include/forge_column.h
@@ -15,5 +15,7 @@ void forge_i64_column_free(forge_i64_column *column);
 int forge_i64_column_reserve(forge_i64_column *column, size_t capacity);
 int forge_i64_column_append(forge_i64_column *column, int64_t value);
 int64_t forge_i64_column_sum(const forge_i64_column *column);
+int forge_i64_column_min(const forge_i64_column *column, int64_t *value);
+int forge_i64_column_max(const forge_i64_column *column, int64_t *value);
 
 #endif
